0b8fe4795ddc08457200649f8850fc02f4b1aa51
EugenefedorovPro <eugene.proskulikov@gmail.com>
2022-12-06 18:05:45 +0200

    tests corrected

--- tests/test_rhyme.py
+++ tests/test_rhyme.py
@@ -4,4 +4,4 @@ from rhyme_rus.rhyme import rhyme
 
 def test_rhyme():
     with open("tests/test_rhyme/test_rhyme.pkl", "rb") as f:
-        assert bool(True) == dill.load(f).equals(rhyme("кот", list_score_numbers=[5]))
+        assert bool(True) == dill.load(f).equals(rhyme("ко'т", list_score_numbers=[5]))
