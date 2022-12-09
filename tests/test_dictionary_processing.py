3aac7b6542936b5c9a6d039ee91b486de96e7f0a
EugenefedorovPro <eugene.proskulikov@gmail.com>
2022-12-09 20:39:35 +0200

    new dictionary_processing tests add, wiktionary.sql rm

---
+++ tests/test_dictionary_processing.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import dill
+from rhyme_rus.utils.dictionary_processing import DictionaryProcessing
+
+
+def test_get_dict_word_accent():
+    dict_word_accent = DictionaryProcessing.get_dict_word_accent()
+    stressed_word = "по'чва"
+    assert dict_word_accent["почва"] == stressed_word
+
+
+def test_get_unique_of_all_int_from_dict():
+    unique_of_all_int_from_dict = DictionaryProcessing.get_unique_of_all_int_from_dict()
+    proper_tuple = (58, 12, 16, 48, 12)
+    assert proper_tuple in unique_of_all_int_from_dict
+
+
+def test_make_dict_of_int_from_ipa():
+    dict_of_int_from_ipa = DictionaryProcessing.make_dict_of_int_from_ipa()
+    path = (
+        Path.cwd()
+        / "tests/test_dictionary_processing/test_make_dict_of_int_from_ipa.pkl"
+    )
+    with open(path, "rb") as f:
+        proper_output = dill.load(f)
+    test_tuple = (58, 12, 16, 48, 12)
+    function_result = dict_of_int_from_ipa[test_tuple]
+    assert vars(proper_output[10]) == vars(function_result[10])
