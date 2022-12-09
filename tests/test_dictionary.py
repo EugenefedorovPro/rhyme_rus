a199b02ddb6cb6be76f4d80d6e35af8e74b52ab3
EugenefedorovPro <eugene.proskulikov@gmail.com>
2022-12-09 20:39:35 +0200

    new dictionary_processing tests add, wiktionary.sql rm

--- tests/test_dictionary.py
+++
@@ -1,7 +0,0 @@
-from wiktionary_rus.wiktionary import wiki_instances, find_item_from_wiki
-
-
-def test_unpack_wiki_parsed():
-    print("inside_test_unpack_wiki_parsed")
-    assert 422821 == len(wiki_instances)
-    assert "dom" == find_item_from_wiki("до'м")[0].sounds
