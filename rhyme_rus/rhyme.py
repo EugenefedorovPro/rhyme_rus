95e45570c8bd2267a00aade7f9fef8c4a2f1a0ac
EugenefedorovPro <eugene.proskulikov@gmail.com>
2023-03-21 15:37:42 +0200

    rhyme as module

--- rhyme_rus/rhyme.py
+++ rhyme_rus/rhyme.py
@@ -1,35 +1,36 @@
+import multiprocessing
+
+import pandas as pd
 from multiprocessing import Pool
-from time import perf_counter
 from rhyme_rus.utils.word import Word
 from rhyme_rus.utils.procedure import Procedure
 from rhyme_rus.utils.split_all_intipa_words import SplitIntipaWords
 from rhyme_rus.utils.concat_tables import concat_tables
 
-target_word = "облако"
-word = Word(target_word)
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
+pd.set_option("display.max_colwidth", None)
 
-if __name__ == "__main__":
-    word = Procedure(word).build_till_intipa_words()
+multiprocessing.set_start_method('spawn')
 
-    split_intipa_words: list[dict[tuple[int]:list[str, ...]], ...]
-    split_intipa_words = SplitIntipaWords(
-        word.all_stresses,
-        word.stressed_word,
-        word.intipa,
-        word.stressed_vowel,
-        word.near_stressed_v,
-        word.index_stressed_v,
-        word.all_intipa_words
-        ).split_intipa_words()
 
-    with Pool() as p:
-        word_instances = p.starmap(Procedure(word).build_split_intipa_words, split_intipa_words)
-        united_table = concat_tables(word_instances)
-        united_table.to_csv(f"{target_word}_mult.csv")
-
-    target_word = "облако"
-    word_one = Word(target_word)
-    word_one = Procedure(word_one).build()
-    word_one.table.to_csv(f"{target_word}_one.csv")
-
-    print(united_table["rhyme"].equals(word_one.table["rhyme"]))
+def rhyme(target_word, stressed_word = ''):
+    if __name__ == "rhyme_rus.rhyme":
+        word = Word(target_word)
+        word.stressed_word = stressed_word
+        word = Procedure(word).build_till_intipa_words()
+        split_intipa_words: list[dict[tuple[int]:list[str, ...]], ...]
+        split_intipa_words = SplitIntipaWords(
+            word.all_stresses,
+            word.stressed_word,
+            word.intipa,
+            word.stressed_vowel,
+            word.near_stressed_v,
+            word.index_stressed_v,
+            word.all_intipa_words
+            ).split_intipa_words()
+        with Pool() as p:
+            word_instances = p.starmap(Procedure(word).build_split_intipa_words, split_intipa_words)
+            united_table = concat_tables(word_instances)
+        return united_table
